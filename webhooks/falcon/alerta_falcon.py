258faf4c2a66efb2e021444687aac6ce87ef53a2
Kevin Cai 蔡雄 <kevin.cai@nio.com>
2018-12-25 22:49:08 +0800

    version 1.0.1

--- webhooks/falcon/alerta_falcon.py
+++ webhooks/falcon/alerta_falcon.py
@@ -2,23 +2,28 @@
 from alerta.models.alert import Alert
 from alerta.webhooks import WebhookBase
 import json
+import time
+import datetime
+
 
 class FalconWebhook(WebhookBase):
 
     def incoming(self, query_string, payload):
 
         # Default parameters
-        environment = 'Production'
+        environment = query_string.get('environment', 'prod')
+        region = query_string.get('region', 'bjaws')
         severity = query_string["priority"]
         status = query_string["status"]
         group = 'Falcon'
+        tags = query_string["tags"].split(',')
         resource = query_string["endpoint"]
         event = query_string["metric"]
-        service = 'Falcon'
+        service = ['Falcon']
         text = 'nothing'
-        tags = query_string["endpoint"].split(',')
-        attributes = {}
-        origin = ''
+        attributes = {"region": region}
+        origin = 'Falcon'
+        create_time = self.formatTime(query_string["time"])
 
         if status == 'OK':
             severity = 'ok'
@@ -29,6 +34,7 @@ class FalconWebhook(WebhookBase):
             environment=environment,
             severity=severity,
             service=service,
+            create_time=create_time,
             group=group,
             value=query_string["left_value"],
             text=text,
@@ -37,3 +43,7 @@ class FalconWebhook(WebhookBase):
             origin=origin,
             raw_data=json.dumps(query_string, indent=4)
         )
+
+    @classmethod
+    def formatTime(cls, s):
+        return int(time.mktime(datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S").timetuple()))
