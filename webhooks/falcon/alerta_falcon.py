9579efaa1cbba3be45e8fdee12ba1755b6cf32a4
Kevin Cai 蔡雄 <kevin.cai@nio.com>
2018-12-26 11:02:09 +0800

    add traceback.print_exc()

--- webhooks/falcon/alerta_falcon.py
+++ webhooks/falcon/alerta_falcon.py
@@ -2,6 +2,7 @@
 import json
 import time
 import datetime
+import traceback
 from alerta.models.alert import Alert
 from alerta.webhooks import WebhookBase
 
@@ -10,39 +11,42 @@ class FalconWebhook(WebhookBase):
 
     def incoming(self, query_string, payload):
 
-        # Default parameters
-        environment = payload.get('environment', 'prod')
-        region = payload.get('region', 'bjaws')
-        severity = payload["priority"]
-        status = payload["status"]
-        group = 'Falcon'
-        tags = payload["tags"].split(',')
-        resource = payload["endpoint"]
-        event = payload["metric"]
-        service = ['Falcon']
-        text = payload["text"]
-        attributes = {"region": region}
-        origin = 'Falcon'
-        create_time = self.formatTime(payload["time"])
+        try:
+            # Default parameters
+            environment = payload.get('environment', 'prod')
+            region = payload.get('region', 'bjaws')
+            severity = payload["priority"]
+            status = payload["status"]
+            group = 'Falcon'
+            tags = payload["tags"].split(',')
+            resource = payload["endpoint"]
+            event = payload["metric"]
+            service = ['Falcon']
+            text = payload["text"]
+            attributes = {"region": region}
+            origin = 'Falcon'
+            create_time = self.formatTime(payload["time"])
 
-        if status == 'OK':
-            severity = 'ok'
+            if status == 'OK':
+                severity = 'ok'
 
-        return Alert(
-            resource=resource,
-            event=event,
-            environment=environment,
-            severity=severity,
-            service=service,
-            create_time=create_time,
-            group=group,
-            value=query_string["left_value"],
-            text=text,
-            tags=tags,
-            attributes=attributes,
-            origin=origin,
-            raw_data=json.dumps(payload, indent=4)
-        )
+            return Alert(
+                resource=resource,
+                event=event,
+                environment=environment,
+                severity=severity,
+                service=service,
+                create_time=create_time,
+                group=group,
+                value=payload["left_value"],
+                text=text,
+                tags=tags,
+                attributes=attributes,
+                origin=origin,
+                raw_data=json.dumps(payload, indent=4)
+            )
+        except:
+            traceback.print_exc()
 
     @classmethod
     def formatTime(cls, s):
